cd794f64dfb07ffa463448846f3f4a1cf7123059
Bence Rozsa <robence@gmail.com>
2022-01-04 12:54:40 +0100

    add challenge content

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,11 +13,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Ask Me Anything'),
     );
   }
 }
@@ -28,9 +31,48 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Ball'),
+        title: Text(title),
+        backgroundColor: Colors.blue[800],
       ),
-      body: Container(),
+      body: Container(
+        color: Colors.blue,
+        child: Center(
+          child: Row(
+            children: const [
+              Expanded(
+                child: BallWidget(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BallWidget extends StatefulWidget {
+  const BallWidget({Key? key}) : super(key: key);
+
+  @override
+  _BallWidgetState createState() => _BallWidgetState();
+}
+
+class _BallWidgetState extends State<BallWidget> {
+  int answerNo = 1;
+
+  void updateAnswer() {
+    setState(() {
+      answerNo = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {
+        updateAnswer();
+      },
+      child: Image.asset('images/ball$answerNo.png'),
     );
   }
 }
